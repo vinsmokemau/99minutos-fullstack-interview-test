57f14e6a8d01b3c0ccf1877337dfa48b84a374d9
Vinsmoke Mau <maumg1196@gmail.com>
2021-01-01 04:11:02 -0600

    Create gitpython test script

---
+++ test.py
@@ -0,0 +1,20 @@
+from git import Repo
+
+
+repo = Repo('.')
+
+branches = repo.heads
+for branch in branches:
+    print(f'Branch: {branch.name}\n')
+
+commits = repo.iter_commits(branch.name)
+previous_commit = ''
+for commit in commits:
+    print(f'Id: {commit}')
+    files = repo.git.show('--name-only', '--pretty=', f'{commit}')
+    no_files = len(files.split('\n'))
+    print(f'Number of files changed: {no_files}')
+    print(f'Author: {commit.author.name}')
+    print(f'Mail: {commit.author.email}')
+    print(f'Message: {commit.message}')
+    print(f'Timestamp: {commit.authored_date}\n')
