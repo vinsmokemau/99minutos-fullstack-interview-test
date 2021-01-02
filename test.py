ea3d95e6b89e7053a1be3c6313cfda93569c58b2
Vinsmoke Mau <maumg1196@gmail.com>
2021-01-01 21:17:46 -0600

    Add lines to test.py

--- test.py
+++ test.py
@@ -18,3 +18,13 @@ for commit in commits:
     print(f'Mail: {commit.author.email}')
     print(f'Message: {commit.message}')
     print(f'Timestamp: {commit.authored_date}\n')
+
+commit = repo.commit(f'{commit}')
+print(f'Id: {commit}')
+files = repo.git.show('--name-only', '--pretty=', f'{commit}')
+no_files = len(files.split('\n'))
+print(f'Number of files changed: {no_files}')
+print(f'Author: {commit.author.name}')
+print(f'Mail: {commit.author.email}')
+print(f'Message: {commit.message}')
+print(f'Timestamp: {commit.authored_date}\n')
